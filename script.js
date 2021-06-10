1e5a7ab5b8290f4969b3cea1ea90cfd0ac1d9629
iikim <iikim2511@gmail.com>
2021-06-10 11:26:41 -0400

    🦄js-4
    /* 🦄js-4
    1 create element  div ol li
    2 when click 'submit' create element    */

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
 console.log("test")
 
-
+// 🍄 const
 const todoContainer = document.querySelector('.todo_container');
 const inputBox = document.querySelector('.input_box');
+
 const input = document.querySelector('.input');
 const todoSubmit = document.querySelector('.todo_submit');
 const todoClearAll = document.querySelector('.todo_clear_all');
-
-/* 
-1  submit
-2 create element  div ol li 
-3 when click 'submit' create element
-4
-*/
 
 
 
 /* 
 1 when click 'clear all'
 */
+
 /* 
 1.  x : clear
 
 2. done : line over text
 */
 
+// 🍄 addeventlistener
+// 2
+todoSubmit.addEventListener('click',todolist);
 
-/* ⚡create */
-let div = document.createElement('div');
-div.className = 'todo';
-document.body.append(div);
+todoClearAll.addEventListener('click',todolist);
 
-let ol = document.createElement('ol');
-ol.className = 'todo_ol';
-div.append(ol);
+// 🍄 function
 
-let li = document.createElement('li');
-li.className = 'todo_li';
-li.innerHTML = 'test'; //input box submitted
-ol.append(li);
+/* 🦄js-4
+1 create element  div ol li 
+2 when click 'submit' create element    */
 
-let button = document.createElement('button');
-button.className = 'todo_delete';
-button.innerHTML = 'x'; 
-li.append(button);
+function todolist(params) {    
+    /* ⚡create */
+    let div = document.createElement('div');
+    div.className = 'todo';
+    document.body.append(div);
 
-let button2 = document.createElement('button');
-button2.className = 'todo_done';
-button2.innerHTML = 'done'; 
-li.append(button2);
+    let ul = document.createElement('ul');
+    ul.className = 'todo_ul';
+    div.append(ul);
+
+    let li = document.createElement('li');
+    li.className = 'todo_li';
+    li.innerHTML = 'test'; //input box submitted
+    ul.append(li);
+
+    let button = document.createElement('button');
+    button.className = 'todo_delete';
+    button.innerHTML = 'x'; 
+    li.append(button);
+
+    let button2 = document.createElement('button');
+    button2.className = 'todo_done';
+    button2.innerHTML = 'done'; 
+    li.append(button2);    
+}
+
+
+
+
 
 
